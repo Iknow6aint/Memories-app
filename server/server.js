e1a47d2ebd946ff21a347cbc1d235c5ca24acb23
iknow6aint <saintagbukor@gmil.com>
2023-01-19 11:48:37 +0100

    legassy init

--- server/server.js
+++ server/server.js
@@ -1,2 +1,11 @@
 import cors from "cors"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import express from "express"
 
+const app = express();
+const PORT = 5000
+
+app.listen(PORT, () => {
+    console.log(`Listening at ${PORT}`);
+})
