f4ac4dbf76937f28ddf860f3d98760ed27b4ab46
iknow6aint <saintagbukor@gmil.com>
2023-01-19 14:09:43 +0100

    posts controler

---
+++ server/routes/posts.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { getPosts, createPost } from '../controllers/posts.js'
+
+const postRouter = express.Router()
+
+postRouter.get('/', getPosts);
+postRouter.post('/', createPost);
+
+
+export default postRouter;
--- server/server.js
+++ server/server.js
@@ -3,9 +3,26 @@ import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import express from "express"
 
+import postRoutes from './routes/posts.js'
+
 const app = express();
 const PORT = 5000
 
-app.listen(PORT, () => {
-    console.log(`Listening at ${PORT}`);
-})
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//routes init
+app.use('/posts', postRoutes)
+
+//mongoDB init
+const CONNECTION_URL = 'mongodb+srv://iknowsaint:jajabone@cluster0.qzlrfzl.mongodb.net/?retryWrites=true&w=majority';
+mongoose.set("strictQuery", false);
+mongoose.connect(CONNECTION_URL, () => {
+    console.log(`Connected to MongoDB`)
+    app.listen(PORT, () => {
+        console.log(`Listening at ${PORT}`);
+    })
+});
