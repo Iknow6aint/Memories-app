29cc841c48d233b7824a17e4722434dddd5b908e
iknow6aint <saintagbukor@gmil.com>
2023-01-19 22:39:05 +0100

    app styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-
 import memories from './images/memories.png'
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Forms/Form";
+
+import useStyles from './styles'
 const App = () => {
+    const classes = useStyles();
     return (
         <Container maxWidth='lg'>
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">
-                    Memonries
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">
+                    memories
                 </Typography>
-                <img src={memories} alt="memories" height="60" />
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
