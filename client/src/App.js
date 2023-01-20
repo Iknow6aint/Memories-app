e0683a150e7165d075990e2138cdca8bc985df13
iknow6aint <saintagbukor@gmil.com>
2023-01-20 11:54:26 +0100

    Post reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import memories from './images/memories.png'
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Forms/Form";
 
+import { getPosts } from './actions/posts'
 import useStyles from './styles'
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [dispatch])
     return (
         <Container maxWidth='lg'>
             <AppBar className={classes.appBar} position="static" color="inherit">
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
 import React from "react"
 import ReactDom from "react-dom"
-
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
 import App from './App'
+import thunk from 'redux-thunk';
+import reducers from './reducers'
 
-ReactDom.render(<App />, document.getElementById('root'));
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
