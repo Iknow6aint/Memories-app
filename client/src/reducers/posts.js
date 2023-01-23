414d3b2a29ec58ffbbf1b53632717a2e5903fc33
iknow6aint <saintagbukor@gmail.com>
2023-01-23 09:10:41 +0100

    like post

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -7,6 +7,7 @@ export default (posts = [], action) => {
             return posts.filter((post) => post._id !== action.payload)
         case 'CREATE':
             return [...posts, action.payload];
+        case 'LIKE':
         case 'UPDATE':
             return posts.map((post) => post._id === action.payload._id ? action.payload : post)
         default:
