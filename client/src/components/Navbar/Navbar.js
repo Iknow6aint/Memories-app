91bb64bb8adc2aba44ed4fa1d4d97359e6f3f70a
iknow6aint <saintagbukor@gmail.com>
2023-01-30 02:46:59 +0100

    logout

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
+import * as actionType from '../../constants/actionType';
 
 import memories from '../../images/memories.png';
 
@@ -15,20 +16,28 @@ const Navbar = () => {
     const history = useHistory();
     const classes = useStyles();
     console.log(user);
-    useEffect(() => {
-        const token = user?.token;
 
-        // if (token) {
-        //     const decodedToken = decode(token);
 
-        //     if (decodedToken.exp * 1000 < new Date().getTime()) logout();
-        // }
-
-        setUser(JSON.parse(localStorage.getItem('profile')));
-    }, [location]);
     const logout = () => {
+        dispatch({ type: actionType.LOGOUT });
 
-    }
+        history.push('/auth');
+
+        setUser(null);
+    };
+
+    // useEffect(() => {
+    //     const token = user?.token;
+
+    //     // if (token) {
+    //     //     const decodedToken = decode(token);
+
+    //     //     if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    //     // }
+
+    //     setUser(JSON.parse(localStorage.getItem('profile')));
+    // }, [location]);
+
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className={classes.brandContainer}>
