284479553f4bd2ca7d5b832be85f5cc8c0c04a0b
iknow6aint <saintagbukor@gmail.com>
2023-02-01 17:52:46 +0100

    likes refactor and pagination

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { getPosts } from '../../actions/posts';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid, Paper } from '@material-ui/core';
 import Posts from '../Posts/Posts'
 import Form from "../Forms/Form"
+import Pagination from '../Pagination'
+import useStyles from './styles';
 const Home = () => {
     const [currentId, setCurrentId] = useState(null)
     const dispatch = useDispatch();
+    const classes = useStyles()
 
     useEffect(() => {
         dispatch(getPosts())
@@ -20,6 +23,9 @@ const Home = () => {
                     </Grid>
                     <Grid item xs={12} sm={4}>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        <Paper className={classes.pagination} elevation={6}>
+                            <Pagination />
+                        </Paper>
                     </Grid>
                 </Grid>
             </Container>
