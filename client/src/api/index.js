ce760f0afff5c09994f4dbce66909545e2dc7004
iknow6aint <saintagbukor@gmail.com>
2023-02-03 08:55:07 +0100

    searchQuery api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -11,6 +11,7 @@ API.interceptors.request.use((req) => {
 });
 
 export const fetchPosts = () => API.get('/posts');
+export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`);
 export const createPost = (newPost) => API.post('/posts', newPost);
 export const likedPost = (id) => API.patch(`/posts/${id}/likepost`);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
