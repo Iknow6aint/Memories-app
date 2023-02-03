0d0421073b1023e08f2bacb7e059352004ec9905
iknow6aint <saintagbukor@gmail.com>
2023-02-03 07:55:56 +0100

    search init

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,10 +6,30 @@ import Posts from '../Posts/Posts'
 import Form from "../Forms/Form"
 import Pagination from '../Pagination'
 import useStyles from './styles';
+import { useHistory, useLocation } from 'react-router-dom';
+import ChipInput from 'material-ui-chip-input'
+
+
+// usequery function
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 const Home = () => {
-    const [currentId, setCurrentId] = useState(null)
+    //page query
+    const query = useQuery();
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+
+    const [currentId, setCurrentId] = useState(0);
     const dispatch = useDispatch();
+
+    //search
+    const [search, setSearch] = useState('');
+    const [tags, setTags] = useState([]);
+    const history = useHistory();
+
     const classes = useStyles()
+
 
     useEffect(() => {
         dispatch(getPosts())
