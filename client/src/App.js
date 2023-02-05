72702c1e6f1c57c647f3c21af0a030d2f0dbf097
iknow6aint <saintagbukor@gmail.com>
2023-02-05 03:32:24 +0100

    styles refactor

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
-import PostDetails from "./components/PostDetails";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
     const user = JSON.parse(localStorage.getItem('profile'));
