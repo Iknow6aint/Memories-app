a191454607c7752b6da7bdbc2767c2f594586982
iknow6aint <saintagbukor@gmail.com>
2023-02-05 11:10:27 +0100

    get post server

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express, { Router } from 'express'
-import { getPosts, createPost, updatePost, deletePost, likePost, getPostBySearch } from '../controllers/posts.js'
+import { getPosts, createPost, updatePost, deletePost, likePost, getPostBySearch, getPost } from '../controllers/posts.js'
 import auth from '../middleware/auth.js';
 
 const postRouter = express.Router()
 
 postRouter.get('/search', getPostBySearch);
 postRouter.get('/', getPosts);
+postRouter.get('/:id', getPost);
 postRouter.post('/', auth, createPost);
 postRouter.patch('/:id', auth, updatePost);
 postRouter.delete("/:id", auth, deletePost);
