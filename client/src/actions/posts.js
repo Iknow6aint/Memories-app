9e28832f85999c32757e1ac86d2a53f917f42721
iknow6aint <saintagbukor@gmail.com>
2023-02-06 09:18:59 +0100

    comment action

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -84,14 +84,14 @@ export const likePost = (id) => async (dispatch) => {
     }
 };
 
-// export const commentPost = (value, id) => async (dispatch) => {
-//     try {
-//         const { data } = await api.comment(value, id);
+export const commentPost = (value, id) => async (dispatch) => {
+    try {
+        const { data } = await api.comment(value, id);
 
-//         dispatch({ type: COMMENT, payload: data });
+        dispatch({ type: COMMENT, payload: data });
 
-//         return data.comments;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
+        return data.comments;
+    } catch (error) {
+        console.log(error);
+    }
+};
