9ce915a65d2566032fdb26a2681275199bb25906
iknow6aint <saintagbukor@gmail.com>
2023-02-06 09:20:38 +0100

    comment api call

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -17,6 +17,8 @@ export const createPost = (newPost) => API.post('/posts', newPost);
 export const likedPost = (id) => API.patch(`/posts/${id}/likepost`);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const comment = (value, id) => API.post(`/posts/${id}/commentPost`, { value });
+
 
 export const signIn = (formData) => API.post('/user/signin', formData);
 export const signUp = (formData) => API.post('/user/signup', formData);
