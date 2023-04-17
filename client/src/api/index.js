afebdd7c4cd335e3285d9967d2d25ae3ef9f7527
iknow6aint <saintagbukor@gmail.com>
2023-04-17 21:12:39 +0100

    chage api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000/' });
+const API = axios.create({ baseURL: 'https://run-tnb3.onrender.com/' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
